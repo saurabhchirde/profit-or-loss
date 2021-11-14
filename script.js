c1ab83811a21af7ddce3983c0a7f28bc16fda659
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-14 23:16:19 +0530

    script: ele selector

---
+++ script.js
@@ -0,0 +1,5 @@
+const bp = document.querySelector(".buyPrice");
+const sp = document.querySelector(".sellingPrice");
+const qty = document.querySelector(".qty");
+const btnCal = document.querySelector(".calculate");
+const output = document.querySelector(".output");
