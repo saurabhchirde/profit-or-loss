2e326854056bcc5fccb8412e69b377e42995c59a
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-15 12:05:19 +0530

    final styling

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const qty = document.querySelector(".qty");
 const btnCal = document.querySelector(".calculate");
 const output = document.querySelector(".output");
 const btnClear = document.querySelector(".clear");
-const message = document.querySelector(".meessage");
 
 function start() {
   sp.value = "";
   bp.value = "";
   qty.value = "";
   output.innerText = "";
+  output.classList.add("border");
   output.classList.remove("activeProfit");
   output.classList.remove("activeLoss");
-  //   message.classList.remove("activeBoxLoss");
-  //   message.classList.remove("activeBoxProfit");
+  output.classList.remove("borderProfit");
+  output.classList.remove("borderLoss");
 }
 
 function calProfitOrLoss() {
@@ -26,17 +26,21 @@ function calProfitOrLoss() {
   if (buyValue > 0 && sellValue >= 0) {
     if (buyValue > sellValue) {
       output.classList.remove("activeProfit");
-      //   message.classList.remove("activeBoxProfit");
-      //   message.classList.add("activeBoxLoss");
+      output.classList.remove("borderProfit");
+      output.classList.remove("border");
+      output.classList.add("borderLoss");
       output.classList.add("activeLoss");
+
       output.innerText = `Your loss is ${buyValue - sellValue} , you lost ${
         ((buyValue - sellValue) * 100) / buyValue
       }% in this trade :(`;
     } else {
       output.classList.remove("activeLoss");
-      //   message.classList.remove("activeBoxLoss");
-      //   message.classList.add("activeBoxProfit");
+      output.classList.remove("borderLoss");
+      output.classList.remove("border");
+      output.classList.add("borderProfit");
       output.classList.add("activeProfit");
+
       output.innerText = `Your Profit is ${sellValue - buyValue} , you gain ${
         ((sellValue - buyValue) * 100) / buyValue
       }% in this trade :)`;
